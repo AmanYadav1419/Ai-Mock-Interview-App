de73f6eda1e22a0055af5a286a445d22d7fb80de
unknown <amanyadav16544@gmail.com>
2024-06-08 18:18:23 +0530

    try the button component of shadcn ui library

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="text-center font-bold mt-10">Aman Yadav</h1>
+    <>
+      <h1 className="text-center font-bold mt-10">Aman Yadav</h1>
+      <Button />
+    </>
   );
 }
