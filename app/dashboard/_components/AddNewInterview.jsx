e28563a92edc47095a5091040cf95ef48afa9b62
unknown <amanyadav16544@gmail.com>
2024-06-26 21:41:44 +0530

    created and added dialog,input,textarea section into AddNewInterview.jsx file created ui

---
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -0,0 +1,104 @@
+"use client";
+
+// as this is client compoent as we ae using useState hook so make it client component
+
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+const AddNewInterview = () => {
+// create states to store data 
+  const [openDailog, setOpenDailog] = useState(false);
+  const [jobPosition, setJobPosition] = useState();
+  const [jobDesc, setJobDesc] = useState();
+  const [jobExperience, setJobExperience] = useState();
+
+
+// this function work only on submit
+const onSubmit = (event) => {
+// to prevent reload of page
+    event.preventDefault();
+
+    console.log(jobPosition,jobDesc,jobExperience)
+}
+  return (
+    <div>
+      <div
+        className="p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all"
+        onClick={() => setOpenDailog(true)}
+      >
+        <h2 className="font-semibold text-lg text-center">+ Add New</h2>
+      </div>
+
+      <Dialog open={openDailog}>
+        <DialogContent className="max-w-2xl">
+          <DialogHeader>
+            <DialogTitle className="text-2xl">
+              Tell us more about your job interviwing
+            </DialogTitle>
+            <DialogDescription>
+              <form onSubmit={onSubmit}>
+                <div>
+                  <h2>
+                    Add Detail about your job position/role, Job description and
+                    experinece
+                  </h2>
+
+                  <div className="mt-7 my-3">
+                    <label>Job Role/Job Position</label>
+                    <Input
+                      placeholder="Ex. Full Stack Developer"
+                      required
+                      onChange={(event) => setJobPosition(event.target.value)}
+                    />
+                  </div>
+
+                  <div className="my-3">
+                    <label>Job Description/ Tech Stack (In Short)</label>
+                    <Textarea
+                      placeholder="Reactjs ,Nextjs SpringBoot, Java, Nodejs, Expressjs,Angular, mysql,Mongodb"
+                      required
+                      onChange={(event) => setJobDesc(event.target.value)}
+                    />
+                  </div>
+
+                  <div className="my-3">
+                    <label>Years of Experience</label>
+                    <Input
+                      placeholder="Ex. 5"
+                      type="number"
+                      max="50"
+                      required
+                      onChange={(event) => setJobExperience(event.target.value)}
+                    />
+                  </div>
+                </div>
+
+                <div className="flex gap-5 justify-end">
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    onClick={() => setOpenDailog(false)}
+                  >
+                    Cancel
+                  </Button>
+                  <Button type="submit">Start Interview</Button>
+                </div>
+              </form>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AddNewInterview;
