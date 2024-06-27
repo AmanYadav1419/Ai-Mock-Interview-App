cdf9fa528394ce40f1c6451dfa1ea71804f62e1b
unknown <amanyadav16544@gmail.com>
2024-06-27 12:01:00 +0530

    added gemini ai api & model , store to db , added uuid,moment packages

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -13,22 +13,94 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { chatSession } from "@/utils/GeminiAIModal";
+import { LoaderCircle } from "lucide-react";
+import { db } from "@/utils/db";
+import { MockInterview } from "@/utils/schema";
+import { v4 as uuidv4 } from "uuid";
+import { useUser } from "@clerk/nextjs";
+import moment from "moment/moment";
 
 const AddNewInterview = () => {
-// create states to store data 
+  // create states to store data
   const [openDailog, setOpenDailog] = useState(false);
   const [jobPosition, setJobPosition] = useState();
   const [jobDesc, setJobDesc] = useState();
   const [jobExperience, setJobExperience] = useState();
+  const [loading, setLoading] = useState(false);
+  const [JsonResponse, setJsonResponse] = useState([]);
 
+  // user details for createdBy
+  const { user } = useUser();
 
-// this function work only on submit
-const onSubmit = (event) => {
-// to prevent reload of page
+  // this function work only on submit
+  const onSubmit = async (event) => {
+    // set loading true as user click the submit button
+    setLoading(true);
+    // to prevent reload of page
     event.preventDefault();
 
-    console.log(jobPosition,jobDesc,jobExperience)
-}
+    // console.log(jobPosition,jobDesc,jobExperience)
+
+    // prompt for gemini api
+    const InputPrompt =
+      "Job Position : " +
+      jobPosition +
+      ", Job Description : " +
+      jobDesc +
+      ", Years of Experience :" +
+      jobExperience +
+      ", Depends on this information please give me " +
+      process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT +
+      " interview Questions with Answered in JSON format , give question and Answered as feild in JSON";
+
+    // call to gemini api
+    const result = await chatSession.sendMessage(InputPrompt);
+
+    // for removing ```json``` from gemini ai response
+    const MockJsonResponse = result.response
+      .text()
+      .replace("```json", "")
+      .replace("```", "");
+
+    // console.log(JSON.parse(MockJsonResponse));
+    // storing all response to state
+    setJsonResponse(MockJsonResponse);
+
+    // to store response to database only if MockJsonResponse is true
+    if (MockJsonResponse) {
+      const resp = await db
+        .insert(MockInterview)
+        // store data in db
+        .values({
+          // unique id generated from uuid package
+          mockId: uuidv4(),
+          jsonMockResp: MockJsonResponse,
+          jobPosition: jobPosition,
+          jobDesc: jobDesc,
+          jobExperience: jobExperience,
+          createdBy: user?.primaryEmailAddress?.emailAddress,
+          //  for date and time we use moment libaray
+          createdAt: moment().format("DD-MM-yyyy"),
+
+          // after this data get inserted it will return mockId
+        })
+        .returning({ mockId: MockInterview.mockId });
+
+      console.log("Inserted ID :", resp);
+      
+      // to close the dialog after response is true
+      if (resp) {
+        setOpenDailog(false);
+      }
+
+    } else {
+      console.log("ERROR");
+    }
+
+    // once finish all process set loading false
+    setLoading(false);
+  };
   return (
     <div>
       <div
@@ -90,7 +162,15 @@ const onSubmit = (event) => {
                   >
                     Cancel
                   </Button>
-                  <Button type="submit">Start Interview</Button>
+                  <Button type="submit" disabled={loading}>
+                    {/* for loading if true then show loading ui */}
+                    {
+                      <>
+                        loading ? <LoaderCircle className="animate-spin" />{" "}
+                        'Generating From AI' : 'Start Interview'
+                      </>
+                    }
+                  </Button>
                 </div>
               </form>
             </DialogDescription>
